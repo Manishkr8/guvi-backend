9ecd716ee1e0bf048e0df73e0bf451e119a2be2d
Manishkr8 <87825636+Manishkr8@users.noreply.github.com>
2023-06-03 12:22:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(
 );
 app.use(cookieParser());
 const corsOptions = {
-  origin: "*",
+  origin: "process.env.FRONTEND",
   credentials: true, //access-control-allow-credentials:true
   methods: ["GET", "POST", "PUT", "DELETE"],
 };
